280fd2cd263f3a8ae144429b51cb394edbb204cd
Minhaz <minhazrabbi93041@gmail.com>
2022-08-19 12:49:13 +0600

    feat: sidebar section completed

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,18 +1,50 @@
-import { Paper, Stack, Typography } from "@mui/material";
+import { Paper, Stack, Typography, Box } from "@mui/material";
 import { Dry } from "@mui/icons-material";
 import CustomButton from "../button/CustomButton";
+
+const paperStyles = {
+	alignItems: "center",
+	justifyContent: "space-between",
+	borderRadius: "14px",
+	p: 4,
+	backgroundColor: "#edf2f7",
+	height: "100%",
+	display: "flex",
+	flexDirection: "column",
+	height: "100%",
+};
+
 const Rightbar = () => {
 	return (
-		<Paper elevation={0} sx={{ borderRadius: "14px", p: 4, backgroundColor: "#edf2f7" }}>
-			<Dry sx={{ color: "#9f7aea", fontSize: 40 }} />
-			<Typography>Best Networking Workshops</Typography>
-			<Typography>
-				In this workshop, learn when and how to network with modern best practices.
-			</Typography>
+		<Paper elevation={0} sx={paperStyles}>
 			<Stack>
-				<Typography>Explain the importance of networking for career-building</Typography>
-				<Typography>Practice modern tips for effective networking</Typography>
-				<Typography>Understand the steps towards building your own network</Typography>
+				<Dry sx={{ color: "#9f7aea", fontSize: 50, mb: "20px" }} />
+				<Typography
+					fontSize={26}
+					fontWeight="bold"
+					variant="h4"
+					mb={3}
+					sx={{ color: "#718096" }}
+				>
+					Best Networking Workshops
+				</Typography>
+				<Typography sx={{ color: "#718096", lineHeight: "20px", mb: "22px" }}>
+					In this workshop, learn when and how to network with modern best practices.
+				</Typography>
+				<Stack sx={{ gap: 1 }}>
+					<Typography sx={{ color: "#718096", fontSize: "15px" }}>
+						<span className="list-dot"></span>Explain the importance of networking for
+						career-building
+					</Typography>
+					<Typography sx={{ color: "#718096", fontSize: "15px" }}>
+						<span className="list-dot"></span>Practice modern tips for effective
+						networking
+					</Typography>
+					<Typography sx={{ color: "#718096", fontSize: "15px" }}>
+						<span className="list-dot"></span>Understand the steps towards building your
+						own network
+					</Typography>
+				</Stack>
 			</Stack>
 			<Stack>
 				<CustomButton link="/our-team" text="Learn More" />
